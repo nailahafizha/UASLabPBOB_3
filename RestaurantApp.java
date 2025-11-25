766b736ff61b59ad6212131b964a63731a5b4797
suciwrnd <suciwrnd@gmail.com>
2025-11-25 10:13:38 +0700

    Create RestaurantApp.java

---
+++ RestaurantApp.java
@@ -0,0 +1,8 @@
+package UASLabPBOB_3;
+
+public class RestaurantApp {
+    public static void main(String[] args) {
+        RestaurantSystem system = new RestaurantSystem();
+        new AppFrame(system);
+    }
+}
