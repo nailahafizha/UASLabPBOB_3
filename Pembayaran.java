2ad9ca355b9e06252c63b9ae17d9ef29422823f2
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-25 10:54:03 +0700

    Update Pembayaran.java

--- Pembayaran.java
+++ Pembayaran.java
@@ -2,7 +2,7 @@ package UASLabPBOB_3;
 
 import java.util.Scanner;
 
-// Interface Pembayaran. Kontrak untuk semua metode pembayaran
+// Interface Pembayaran (Kontrak untuk semua metode pembayaran)
 public interface Pembayaran {
 
     // Memproses transaksi dan mengembalikan status keberhasilan (true/false)
